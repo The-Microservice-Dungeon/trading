583ea8edf07e52383feb201e13e64a542d6396ce
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2022-04-07 12:24:39 +0200

    refactor: unsafe optional usage (fixes #10)

--- src/main/java/dungeon/trading/game/GameService.java
+++ src/main/java/dungeon/trading/game/GameService.java
@@ -4,113 +4,115 @@ import dungeon.trading.item.ItemService;
 import dungeon.trading.player.PlayerService;
 import dungeon.trading.resource.ResourceService;
 import dungeon.trading.station.StationService;
-import net.minidev.json.JSONArray;
-import net.minidev.json.JSONObject;
-import org.apache.kafka.common.quota.ClientQuotaAlteration;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
+import javax.transaction.Transactional;
+import net.minidev.json.JSONArray;
+import net.minidev.json.JSONObject;
+import org.springframework.stereotype.Service;
 
 @Service
 public class GameService {
 
-    private final GameRepository gameRepository;
+  private final GameRepository gameRepository;
 
-    private final PlayerService playerService;
+  private final PlayerService playerService;
 
-    private final ItemService itemService;
+  private final ItemService itemService;
 
-    private final ResourceService resourceService;
+  private final ResourceService resourceService;
 
-    private final StationService stationService;
+  private final StationService stationService;
 
-    public GameService(GameRepository gameRepository,
-        PlayerService playerService, ItemService itemService, ResourceService resourceService,
-        StationService stationService) {
-        this.gameRepository = gameRepository;
-        this.playerService = playerService;
-        this.itemService = itemService;
-        this.resourceService = resourceService;
-        this.stationService = stationService;
+  public GameService(GameRepository gameRepository,
+      PlayerService playerService, ItemService itemService, ResourceService resourceService,
+      StationService stationService) {
+    this.gameRepository = gameRepository;
+    this.playerService = playerService;
+    this.itemService = itemService;
+    this.resourceService = resourceService;
+    this.stationService = stationService;
+  }
+
+  @Transactional
+  public void createNewGame(UUID newGameId) {
+    Optional<Game> currentGame = this.gameRepository.findByIsCurrentGame(true);
+    if (currentGame.isPresent()) {
+      currentGame.get().stopGame();
+      this.itemService.resetItems();
+      this.resourceService.resetResources();
+      this.playerService.removePlayers();
+      this.stationService.removeStations();
     }
 
-    @Transactional
-    public void createNewGame(UUID newGameId) {
-        Optional<Game> currentGame = this.gameRepository.findByIsCurrentGame(true);
-        if (currentGame.isPresent()) {
-            currentGame.get().stopGame();
-            this.itemService.resetItems();
-            this.resourceService.resetResources();
-            this.playerService.removePlayers();
-            this.stationService.removeStations();
+    Game newGame = new Game(newGameId);
+    newGame.startGame();
+    this.gameRepository.save(newGame);
+  }
+
+  @Transactional
+  public void stopGame(UUID gameId) {
+    Optional<Game> game = this.gameRepository.findById(gameId);
+    game.ifPresent(Game::stopGame);
+  }
+
+  @Transactional
+  public void updateRound(RoundDto roundDto) {
+    if (roundDto.roundNumber == 1) {
+        if (this.itemService.getItems().isEmpty()) {
+            this.itemService.createAllItems();
         }
-
-        Game newGame = new Game(newGameId);
-        newGame.startGame();
-        this.gameRepository.save(newGame);
-    }
-
-    @Transactional
-    public void stopGame(UUID gameId) {
-        Optional<Game> game = this.gameRepository.findById(gameId);
-        game.ifPresent(Game::stopGame);
-    }
-
-    @Transactional
-    public void updateRound(RoundDto roundDto) {
-        if (roundDto.roundNumber == 1) {
-            if (this.itemService.getItems().isEmpty()) this.itemService.createAllItems();
-            if (this.resourceService.getResources().isEmpty()) this.resourceService.createResources();
+        if (this.resourceService.getResources().isEmpty()) {
+            this.resourceService.createResources();
         }
-
-        if (Objects.equals(roundDto.roundStatus, "ended")) {
-            this.playerService.updatePlayerBalanceHistories(roundDto.roundNumber);
-        } else if (Objects.equals(roundDto.roundStatus, "started")) {
-            this.itemService.calculateNewItemPrices();
-            this.resourceService.calculateNewResourcePrices();
-        }
-
-        Optional<Game> newGame = this.gameRepository.findById(UUID.fromString(roundDto.gameId));
-        newGame.get().updateRoundCount(roundDto.roundNumber);
-        newGame.get().updateRoundStatus(roundDto.roundStatus);
     }
 
-    public int getRoundCount() {
-        Optional<Game> newGame = this.gameRepository.findByIsCurrentGame(true);
-        return newGame.get().getCurrentRound();
+    if (Objects.equals(roundDto.roundStatus, "ended")) {
+      this.playerService.updatePlayerBalanceHistories(roundDto.roundNumber);
+    } else if (Objects.equals(roundDto.roundStatus, "started")) {
+      this.itemService.calculateNewItemPrices();
+      this.resourceService.calculateNewResourcePrices();
     }
 
-    public String getRoundStatus() {
-        Optional<Game> newGame = this.gameRepository.findByIsCurrentGame(true);
-        return newGame.get().getRoundStatus();
+    Optional<Game> newGame = this.gameRepository.findById(UUID.fromString(roundDto.gameId));
+    var game = newGame.orElseThrow();
+    game.updateRoundCount(roundDto.roundNumber);
+    game.updateRoundStatus(roundDto.roundStatus);
+  }
+
+  public int getRoundCount() {
+    Game newGame = this.gameRepository.findByIsCurrentGame(true).orElseThrow();
+    return newGame.getCurrentRound();
+  }
+
+  public String getRoundStatus() {
+    Game newGame = this.gameRepository.findByIsCurrentGame(true).orElseThrow();
+    return newGame.getRoundStatus();
+  }
+
+  public UUID getCurrentGameId() {
+    Game newGame = this.gameRepository.findByIsCurrentGame(true).orElseThrow();
+    return newGame.getGameId();
+  }
+
+  public Iterable<Game> getGames() {
+    return this.gameRepository.findAll();
+  }
+
+  public JSONArray getAllGames() {
+    JSONArray array = new JSONArray();
+
+    for (Game game : this.gameRepository.findAll()) {
+      JSONObject object = new JSONObject();
+      object.put("gameId", game.getGameId());
+      object.put("isCurrentGame", game.getIsCurrentGame());
+      object.put("currentRound", game.getCurrentRound());
+      object.put("roundStatus", game.getRoundStatus());
+      array.add(object);
     }
 
-    public UUID getCurrentGameId() {
-        Optional<Game> newGame = this.gameRepository.findByIsCurrentGame(true);
-        return newGame.get().getGameId();
-    }
-
-    public Iterable<Game> getGames() {
-        return this.gameRepository.findAll();
-    }
-
-    public JSONArray getAllGames() {
-        JSONArray array = new JSONArray();
-
-        for (Game game : this.gameRepository.findAll()) {
-            JSONObject object = new JSONObject();
-            object.put("gameId", game.getGameId());
-            object.put("isCurrentGame", game.getIsCurrentGame());
-            object.put("currentRound", game.getCurrentRound());
-            object.put("roundStatus", game.getRoundStatus());
-            array.add(object);
-        }
-
-        return array;
-    }
+    return array;
+  }
 }
 
