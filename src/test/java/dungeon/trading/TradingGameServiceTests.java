3b0a93b76af3e396d02e7cbaeb586358645e3676
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2022-04-07 12:46:48 +0200

    fix: database cleanup after game end (fixes #9)

--- src/test/java/dungeon/trading/TradingGameServiceTests.java
+++ src/test/java/dungeon/trading/TradingGameServiceTests.java
@@ -1,41 +1,66 @@
 package dungeon.trading;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.mockito.Mockito.verify;
+
+import dungeon.trading.game.Game;
+import dungeon.trading.game.GameRepository;
 import dungeon.trading.game.GameService;
+import dungeon.trading.item.ItemRepository;
 import dungeon.trading.player.PlayerRepository;
+import dungeon.trading.resource.ResourceRepository;
 import dungeon.trading.station.StationRepository;
+import java.util.UUID;
+import javax.transaction.Transactional;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import javax.transaction.Transactional;
-
-import java.util.UUID;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
-public class TradingGameServiceTests {
-    private final GameService gameService;
-    private final PlayerRepository playerRepository;
-    private final StationRepository stationRepository;
+class TradingGameServiceTests {
 
-    @Autowired
-    public TradingGameServiceTests(GameService gameService,
-                                   PlayerRepository playerRepository,
-                                   StationRepository stationRepository) {
-        this.gameService = gameService;
-        this.playerRepository = playerRepository;
-        this.stationRepository = stationRepository;
-    }
+  // It's fine to use field injection in tests
+  @Autowired
+  GameService gameService;
+  @MockBean
+  PlayerRepository playerRepository;
+  @MockBean
+  StationRepository stationRepository;
+  @MockBean
+  ItemRepository itemRepository;
+  @MockBean
+  GameRepository gameRepository;
+  @MockBean
+  ResourceRepository resourceRepository;
 
-    @Test
-    @Transactional
-    public void createGameTest() {
-        this.gameService.createNewGame(UUID.randomUUID());
+  @Test
+  @Transactional
+  void createGameTest() {
+    this.gameService.createNewGame(UUID.randomUUID());
 
-        assertNotEquals(
-                "[]",
-                this.gameService.getGames().toString()
-        );
-    }
+    assertNotEquals(
+        "[]",
+        this.gameService.getGames().toString()
+    );
+  }
+
+  @Test
+  void stopGameShouldClearDatabase() {
+    var gameID = UUID.randomUUID();
+    this.gameService.createNewGame(gameID);
+
+    this.gameService.stopGame(gameID);
+
+    // Might be an idea to not simply test verify invocation. But for now this should work
+    verify(playerRepository).deleteAll();
+    verify(stationRepository).deleteAll();
+    verify(itemRepository).deleteAll();
+    verify(resourceRepository).deleteAll();
+    ArgumentCaptor<Game> gameArgument = ArgumentCaptor.forClass(Game.class);
+    verify(gameRepository).save(gameArgument.capture());
+    assertThat(gameArgument.getValue().getIsCurrentGame()).isFalse();
+  }
 }
